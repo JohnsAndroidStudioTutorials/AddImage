52f58b56b53297436dc3efea9247aa0059f7c979
John Sartain <jhnsartain3@gmail.com>
2016-04-09 13:56:16 -0500

    Initial commit

--- app/src/main/java/com/sartainstudios/addimage/MainActivity.java
+++ app/src/main/java/com/sartainstudios/addimage/MainActivity.java
@@ -2,6 +2,7 @@ package com.sartainstudios.addimage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView myImageView = (ImageView) findViewById(R.id.my_image_view);
+        myImageView.setImageResource(R.drawable.android_background);
     }
 }
